c699946de4c96c127287f060d8d271517e4d858b
Denny <1160345031@qq.com>
2021-03-20 23:46:12 +0800

    83.删除排序链表中的重复元素

---
+++ 83.删除排序链表中的重复元素.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=javascript
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function (head) {
+  if (head === null) return head;
+  var p = head;
+//   while (p) {
+//     if (p.next && p.val === p.next.val) {
+  while (p.next) {
+    if (p.val === p.next.val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return head;
+};
+// @lc code=end
