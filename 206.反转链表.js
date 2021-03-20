3db48c7d2054f73c737e02143ab005a17e4de671
Denny <1160345031@qq.com>
2021-03-20 16:48:33 +0800

    206翻转链表递归

--- 206.反转链表.js
+++ 206.反转链表.js
@@ -20,15 +20,10 @@ var reverseList = function (head) {
   if (!head || !head.next) {
     return head;
   }
-  let pre = null;
-  let cur = head;
-  let next = head.next;
-  while (cur) {
-    cur.next = pre;
-    pre = cur;
-    cur = next;
-    next = next && next.next;
-  }
-  return pre;
+  var tail = head.next;
+  var result = reverseList(head.next);
+  head.next = null;
+  tail.next = head;
+  return result;
 };
 // @lc code=end
